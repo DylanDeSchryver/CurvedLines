acdb71a728d975e462cc6906c6f593a2e46c3e71
DylanDeSchryver <142050322+DylanDeSchryver@users.noreply.github.com>
2024-02-25 20:34:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import cv2
 from lineDetect import contourDetection
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(1) # Initialize Camera
 
 while True:
-    contourDetection(cap)
+    contourDetection(cap) # Line detection
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'): # if q is pressed, windows close and video stream ends. Program exits
         break
